aa2432ca58a7ee47a8a14d09fd6858ea67a3d5f0
Bhaskar Kandiyal <bkandiyal@gmail.com>
2020-03-31 13:14:38 +0530

    Cleanup directory structure

--- scripts.js
+++ scripts.js
@@ -1,24 +1,242 @@
-var URL_CONFIRMED="COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
-var URL_RECOVERED="COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv";
-var URL_DEATHS="COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv";
-
 var DATA = {
-    confirmed: {},
-    recovered: {},
-    active: {},
+    all: [],
     dates: [],
-    summary: {},
+    header: {}
+};
+
+var map = null;
+
+function getDataForDate(data, date) {
+    let ret = [{}];
+    let idx = data[0].indexOf('date');
+    if (idx == -1) return null;
+
+    let obj = {date: date, total_cases: 0, total_recovered:0, total_deaths: 0, total_active: 0};
+
+    for (i in data) {
+        if (i == 0) continue;
+        if (data[i][idx] == date) {
+            ret.push(data[i]);
+            let c = data[i][data[0].indexOf('cases')];
+            let r = data[i][data[0].indexOf('recovered')];
+            let d = data[i][data[0].indexOf('deaths')];
+
+            obj.total_cases += (c == null)?0:c;
+            obj.total_recovered += (r == null)?0:r;
+            obj.total_deaths += (d == null)?0:d;
+        }
+    }
+    obj.total_active = obj.total_cases - obj.total_recovered - obj.total_deaths;
+    ret[0] = obj;
+    return ret;
 }
 
-var svg = d3.select('#globalmap');
+function getDates(data) {
+    let ret = [];
+    let idx = data[0].indexOf('date');
+    for (i in data) {
+        let d = data[i][idx];
+        if (ret.indexOf(d) == -1) ret.push(d);
+    }
+    return ret;
+}
 
-const width = function() {
-    return window.innerWidth;
-};
+function setupMap() {
+    map = new mapboxgl.Map({
+        container: 'map', // container id
+        style: 'mapbox-style.json', // stylesheet location
+        center: [0, 25], // starting position [lng, lat]
+        zoom: 2 // starting zoom
+    });
+    // Add zoom and rotation controls to the map.
+    map.addControl(new mapboxgl.NavigationControl());
+    map.on('load', function() {
+        console.log('Map loaded');
 
-const height = function() {
-    return window.innerHeight;
-};
+        let data = getDataForDate(DATA.all, DATA.dates[DATA.dates.length-1]);
+
+        map.addSource('confirmed', {
+            type: 'geojson',
+            data: processPoints(data, 'cases'),
+        });
+
+        map.addSource('recovered', {
+            type: 'geojson',
+            data: processPoints(data, 'recovered'),
+        });
+
+        map.addSource('deaths', {
+            type: 'geojson',
+            data: processPoints(data, 'deaths'),
+        });
+
+        map.addSource('active', {
+            type: 'geojson',
+            data: processPoints(data, 'active'),
+        });
+
+        map.addLayer({
+            id: 'confirmed-bubbles',
+            type: 'circle',
+            source: 'confirmed',
+            paint: {
+                'circle-radius': [
+                    'step',
+                    ['get', 'percentage'],
+                    6,2,
+                    12,5,
+                    50,
+                    100,100
+                ],
+                'circle-color': 'rgb(255, 0, 0)',
+                'circle-opacity': 0.6,
+                'circle-blur': [
+                    'interpolate', ['linear'], ['get', 'percentage'],
+                    0, 0.5,
+                    100, 0.1
+                ]
+            }
+        });
+
+        map.addLayer({
+            id: 'recovered-bubbles',
+            type: 'circle',
+            source: 'recovered',
+            paint: {
+                'circle-radius': [
+                    'step',
+                    ['get', 'percentage'],
+                    6,2,
+                    12,5,
+                    50,
+                    100,100
+                ],
+                'circle-color': 'rgb(0, 110, 20)',
+                'circle-opacity': 0.6,
+                'circle-blur': [
+                    'interpolate', ['linear'], ['get', 'percentage'],
+                    0, 0.5,
+                    100, 0.1
+                ]
+            }
+        });
+
+        map.addLayer({
+            id: 'deaths-bubbles',
+            type: 'circle',
+            source: 'deaths',
+            paint: {
+                'circle-radius': [
+                    'step',
+                    ['get', 'percentage'],
+                    6,2,
+                    12,5,
+                    50,
+                    100,100
+                ],
+                'circle-color': 'rgb(100, 0, 0)',
+                'circle-opacity': 0.8,
+                'circle-blur': [
+                    'interpolate', ['linear'], ['get', 'percentage'],
+                    0, 0.5,
+                    100, 0.1
+                ]
+            }
+        });
+
+        map.addLayer({
+            id: 'active-bubbles',
+            type: 'circle',
+            source: 'active',
+            paint: {
+                'circle-radius': [
+                    'step',
+                    ['get', 'Percentage'],
+                    6,2,
+                    12,5,
+                    50,
+                    100,100
+                ],
+                'circle-color': 'rgb(255, 208, 20)',
+                'circle-opacity': 0.8,
+                'circle-blur': [
+                    'interpolate', ['linear'], ['get', 'percentage'],
+                    0, 0.5,
+                    100, 0.1
+                ]
+            }
+        });
+
+        let popup = new mapboxgl.Popup({
+            closeButton: false,
+            closeOnClick: false
+        });
+
+        map.on('mouseenter', 'confirmed-bubbles', function(e) {
+            // Change the cursor style as a UI indicator.
+            map.getCanvas().style.cursor = 'pointer';
+            
+            var coordinates = e.features[0].geometry.coordinates.slice();
+            var description = `
+                <ul class="list list-unstyled">
+                    <li class="${(e.features[0].properties.state.length==0)?'d-none':''}">
+                        <strong>State:</strong> ${e.features[0].properties.state}
+                    </li>
+                    <li class="">
+                        <strong>Country:</strong> ${e.features[0].properties.country}
+                    </li>
+                    <li>
+                        <strong>Confirmed:</strong> ${e.features[0].properties.cases}
+                    </li>
+                    <li>
+                        <strong>Coordinates:</strong> ${e.features[0].properties.lat} : ${e.features[0].properties.long}
+                    </li>
+                </ul>
+            `;
+            
+            // Ensure that if the map is zoomed out such that multiple
+            // copies of the feature are visible, the popup appears
+            // over the copy being pointed to.
+            while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+            }
+            
+            // Populate the popup and set its coordinates
+            // based on the feature found.
+            popup
+            .setLngLat(coordinates)
+            .setHTML(description)
+            .addTo(map);
+        });
+
+        map.on('mouseleave', 'confirmed-bubbles', function() {
+            map.getCanvas().style.cursor = '';
+            popup.remove();
+        });
+
+        setupUI();
+    });
+}
+
+$(document).ready(function() {
+    $.ajax({
+        type: 'GET',
+        url: 'https://coronadatascraper.com/timeseries.csv',
+        dataType: 'text',
+        success: function(resp) {
+            console.log('Getting CSV...');
+            DATA.all = $.csv.toArrays(resp, {onParseValue: $.csv.hooks.castToScalar});
+            DATA.dates = getDates(DATA.all);
+            DATA.dates.sort(function(a, b) {
+                return new Date(a) - new Date(b);
+            });
+            for (i in DATA.all[0]) {
+                DATA.header[DATA.all[0][i]] = i;
+            }
+            setupMap();
+        }
+    });
+});
 
 var timer = null;
 
@@ -34,83 +252,69 @@ function stepTimer() {
         $('#date-slider').val(cnt).trigger('change');
 }
 
-function processPoints(data, key) {
+function processPoints(data, type) {
+    console.log('Processing points for ' + type);
     let ret = {type: 'FeatureCollection', features:[]};
-    let total = 0;
-
     for (k in data) {
-        total += data[k].dates[key];
-    }
-
-    for (k in data) {
+        if (k == 0) continue; // Skip summary object
         let e = data[k];
-        if (e.dates[key] == 0) continue;
+        if (e[type] == 0) continue;
+        // console.log(type + ' : ' + e[DATA.header[type]] + ' / ' + data[0]['total_'+type]);
+        // console.log(data[0]['total_'+type]);
+        // console.log(data[0]);
         ret.features.push({
             type: 'Feature',
             properties: {
-                'State':e['Province/State'],
-                'Country':e['Country/Region'],
-                'Value':e.dates[key],
-                'Date':key,
-                'Percentage':+((e.dates[key]/total)*100).toFixed(2)
+                'type':type,
+                'state':e[DATA.header['state']],
+                'country':e[DATA.header['country']],
+                'cases':e[DATA.header['cases']],
+                'recovered':e[DATA.header['recovered']],
+                'deaths':e[DATA.header['deaths']],
+                'active':e[DATA.header['active']],
+                'date':data[0].date,
+                'percentage':+((e[DATA.header[type]]/data[0]['total_'+type])*100).toFixed(2),
+                'lat': e[DATA.header['long']],
+                'long': e[DATA.header['lat']]
             },
             geometry: {
                 type: 'Point',
-                coordinates: [e['Long'], e['Lat']]
+                coordinates: [+e[DATA.header['long']], +e[DATA.header['lat']]]
             }
         });
     }
-    //console.log('Points:');
     // console.log(ret);
     return ret;
 }
 
 function loadData() {
     var date = DATA.dates[$('#date-slider').val()];
+    console.log(date);    
     $('#date').text(date);
 
-    let totalConfirmed = 0;
-    let totalRecovered = 0;
-    let totalDeaths = 0;
-    let totalActive = 0;
+    let data = getDataForDate(DATA.all, date);
 
-    for (c in DATA.confirmed) {
-        totalConfirmed += DATA.confirmed[c].dates[date];
-    }
+    let totalConfirmed = data[0].total_cases;
+    let totalRecovered = data[0].total_recovered;
+    let totalDeaths = data[0].total_deaths;
+    let totalActive = data[0].total_active;
 
-    for (c in DATA.recovered) {
-        totalRecovered += DATA.recovered[c].dates[date];
-    }
-
-    for (c in DATA.deaths) {
-        totalDeaths += DATA.deaths[c].dates[date];
-    }
-
-    for (c in DATA.active) {
-        totalActive += DATA.active[c].dates[date];
-    }
-
-    map.getSource('confirmed').setData(processPoints(DATA.confirmed, date));
-    map.getSource('recovered').setData(processPoints(DATA.recovered, date));
-    map.getSource('deaths').setData(processPoints(DATA.deaths, date));
-    map.getSource('active').setData(processPoints(DATA.active, date));
+    map.getSource('confirmed').setData(processPoints(data, 'cases'));
+    map.getSource('recovered').setData(processPoints(data, 'recovered'));
+    map.getSource('deaths').setData(processPoints(data, 'deaths'));
+    map.getSource('active').setData(processPoints(data, 'active'));
 
     $('#total-confirmed').html(totalConfirmed);
     $('#total-recovered').html(totalRecovered);
     $('#total-deaths').html(totalDeaths);
     $('#total-active').html(totalActive);
     $('#mortality-rate').html(((totalDeaths/totalConfirmed) * 100).toFixed(2)+'%');
-
-    // d3.select('#date').text('Date: ' + date);
-    // d3.select('#total-confirmed').text(totalConfirmed);
-    // d3.select('#total-recovered').text(totalRecovered);
-    // d3.select('#total-deaths').text(totalDeaths);
-    // d3.select('#total-active').text(totalActive);
-    // d3.select('#mortality-rate').text(((totalDeaths/totalConfirmed) * 100).toFixed(2)+'%');
 }
 
 // UI Stuff
 function setupUI() {
+    console.log('Setting up UI');
+
     map.setLayoutProperty('confirmed-bubbles', 'visibility', 'visible');
     map.setLayoutProperty('recovered-bubbles', 'visibility', 'none');
     map.setLayoutProperty('deaths-bubbles', 'visibility', 'none');
@@ -150,219 +354,4 @@ function setupUI() {
     });
 
     $('#date-slider').val(DATA.dates.length-1).trigger('change');
-
-    //d3.select('#date').attr('data-cols', DATA.dates.length);
-    // d3.select('#date-slider')
-    //     .attr('max', DATA.dates.length-1)
-    //     .on('input', function() {
-    //         let v = d3.select(this).node().value;
-    //         d3.select(this).attr('value', v);
-    //         loadData()
-    // });
-    // d3.select('#btnPlay').on('click', function() {
-    //     var cnt = +d3.select('#date-slider').attr('value');
-    //     var total = +d3.select('#date-slider').attr('max');
-    //     setInterval(function() {
-    //         cnt += 1;
-    //         if (cnt > total) return false;
-    //         d3.select('#date-slider').property('value', cnt).attr('value', cnt);
-    //         loadData();
-    //         return true;
-    //     }, 500);
-    // });
-    // d3.select('#dataType').on('change', function(v) {
-    //     var v = d3.select(this).property('value');
-    //     loadData(DATA[v], 0);
-    // });
 }
-
-function formatData(data) {
-    let common_keys = ['Province/State', 'Country/Region', 'Lat', 'Long'];
-    let ret = {};
-    data.forEach(function(d) {
-        let id = ''+d['Lat']+':'+d['Long'];
-        let tmp = {};
-        tmp.dates = d;
-         common_keys.forEach(k => { 
-            tmp[k] = d[k];
-            delete tmp.dates[k];
-        });
-        tmp['Lat'] = +tmp['Lat'];
-        tmp['Long'] = +tmp['Long'];
-        for (k in tmp.dates) {
-            tmp.dates[k] = +tmp.dates[k];
-        }
-        ret[id] = tmp;
-    });
-    return ret;
-}
-
-var map = new mapboxgl.Map({
-    container: 'globalmap', // container id
-    style: 'mapbox-style.json', // stylesheet location
-    center: [0, 25], // starting position [lng, lat]
-    zoom: 2 // starting zoom
-});
-
-$(document).ready(function() {   
-    // Add zoom and rotation controls to the map.
-    map.addControl(new mapboxgl.NavigationControl());
-    
-    console.log('Fetching data....');
-    
-    // Download the required data
-    Promise.all([
-        d3.csv(URL_CONFIRMED),
-        d3.csv(URL_RECOVERED),
-        d3.csv(URL_DEATHS),
-    ]).then(function(data) {
-        DATA.dates = data[0].columns.slice(4, data[0].columns.length);
-    
-        DATA.confirmed = formatData(data[0]);
-        DATA.active = JSON.parse(JSON.stringify(DATA.confirmed)); // Deep copy the data
-        DATA.recovered = formatData(data[1]);
-        DATA.deaths = formatData(data[2]); 
-    
-        for (let k in DATA.active) {
-            let e = DATA.active[k];
-            for (let d in e.dates) {
-                DATA.active[k].dates[d] -= (DATA.recovered[k].dates[d] + DATA.deaths[k].dates[d]);
-            }
-        }
-    
-        let totalConfirmed = 0;
-        let date = DATA.dates[DATA.dates.length-1];
-        console.log(date);
-    
-        for (c in DATA.confirmed) {
-            totalConfirmed += DATA.confirmed[c].dates[date];
-        }
-    
-        map.on('load', function() {
-            console.log('Map loaded');
-    
-            map.addSource('confirmed', {
-                type: 'geojson',
-                data: processPoints(DATA.confirmed, date),
-            });
-    
-            map.addSource('recovered', {
-                type: 'geojson',
-                data: processPoints(DATA.recovered, date),
-            });
-    
-            map.addSource('deaths', {
-                type: 'geojson',
-                data: processPoints(DATA.deaths, date),
-            });
-    
-            map.addSource('active', {
-                type: 'geojson',
-                data: processPoints(DATA.active, date),
-            });
-    
-            map.addLayer({
-                id: 'confirmed-bubbles',
-                type: 'circle',
-                source: 'confirmed',
-                paint: {
-                    'circle-radius': [
-                        'step',
-                        ['get', 'Percentage'],
-                        6,2,
-                        12,5,
-                        50,
-                        100,100
-                    ],
-                    'circle-color': 'rgb(255, 0, 0)',
-                    'circle-opacity': 0.6,
-                    'circle-blur': [
-                        'interpolate', ['linear'], ['get', 'Percentage'],
-                        0, 0.5,
-                        100, 0.1
-                    ]
-                }
-            });
-    
-            map.addLayer({
-                id: 'recovered-bubbles',
-                type: 'circle',
-                source: 'recovered',
-                paint: {
-                    'circle-radius': [
-                        'step',
-                        ['get', 'Percentage'],
-                        6,2,
-                        12,5,
-                        50,
-                        100,100
-                    ],
-                    'circle-color': 'rgb(0, 110, 20)',
-                    'circle-opacity': 0.6,
-                    'circle-blur': [
-                        'interpolate', ['linear'], ['get', 'Percentage'],
-                        0, 0.5,
-                        100, 0.1
-                    ]
-                }
-            });
-    
-            map.addLayer({
-                id: 'deaths-bubbles',
-                type: 'circle',
-                source: 'deaths',
-                paint: {
-                    'circle-radius': [
-                        'step',
-                        ['get', 'Percentage'],
-                        6,2,
-                        12,5,
-                        50,
-                        100,100
-                    ],
-                    'circle-color': 'rgb(100, 0, 0)',
-                    'circle-opacity': 0.8,
-                    'circle-blur': [
-                        'interpolate', ['linear'], ['get', 'Percentage'],
-                        0, 0.5,
-                        100, 0.1
-                    ]
-                }
-            });
-    
-            map.addLayer({
-                id: 'active-bubbles',
-                type: 'circle',
-                source: 'active',
-                paint: {
-                    'circle-radius': [
-                        'step',
-                        ['get', 'Percentage'],
-                        6,2,
-                        12,5,
-                        50,
-                        100,100
-                    ],
-                    'circle-color': 'rgb(255, 208, 20)',
-                    'circle-opacity': 0.8,
-                    'circle-blur': [
-                        'interpolate', ['linear'], ['get', 'Percentage'],
-                        0, 0.5,
-                        100, 0.1
-                    ]
-                }
-            });
-    
-            setupUI();
-        });
-    });
-});
-
-
-
-
-// window.addEventListener("resize", function() {
-//     svg.text('');
-//     renderMap();
-//     loadData();
-// });
