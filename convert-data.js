52bd0a75cc8b129c72273839326acabadbcd0240
Bhaskar Kandiyal <bkandiyal@gmail.com>
2020-04-03 00:57:57 +0530

    Major changes

---
+++ convert-data.js
@@ -0,0 +1,33 @@
+/**
+ * Converts JHU data CSV files to month-wise JSON
+ */
+const fs = require('fs');
+const path = require('path');
+const csv = require('fast-csv');
+
+function addZ(n) { return (n<10)?'0'+n:''+n; }
+
+var data = {};
+
+function processRow(row) {
+    return [row[0], {
+            'Confirmed': +row[4],
+            'Recovered': +row[2],
+            'Deaths': +row[3]
+    }];
+}
+
+var timestamp_data = JSON.parse(fs.readFileSync('data/timeseries_india.json'));
+timestamp_data['Dates'] = {};
+
+let cnt = -1;
+
+csv.parseString(fs.readFileSync('covid_19_india.csv'))
+    .on('data', (row) => {
+        cnt += 1;
+        if (cnt == 0) return;
+        r = processRow(row);
+        if (!timestamp_data['Dates'].hasOwnProperty(r[0])) timestamp_data['Dates'][r[0]] = {};
+        timestamp_data['Dates'][r[0]][row[1]+'/India'] = r[1];
+    })
+    .on('end', () => fs.writeFileSync('data/timeseries_india.json', JSON.stringify(timestamp_data)));
---
+++ fetch-data.js
@@ -0,0 +1,78 @@
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const d3 = require('d3');
+const csv = require('fast-csv');
+
+function addZ(n) { return (n<10)?'0'+n:''+n; }
+
+axios.all([
+    axios.get('https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'),
+    axios.get('https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv'),
+    axios.get('https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv'),
+    axios.get('https://www.mohfw.gov.in/')
+]).then(axios.spread((c, r, d, moh) => {
+    let india_timeseries = JSON.parse(fs.readFileSync('data/timeseries_india.json'));
+
+    var $ = cheerio.load(moh.data);
+    let date = new Date($('#site-dashboard .status-update span').text().split(':').slice(1)[0].trim().split(',')[0]);
+    let t = addZ(date.getDate())+'-'+addZ((date.getMonth()+1))+'-'+date.getFullYear();
+    console.log('Date: ' + t);
+
+    $('#state-data .data-table table tbody').find('tr').each(function(i, e) {
+        let children = $(e).children();
+        if (isNaN(parseInt($(children[0]).text()))) return;
+        if (!india_timeseries['Dates'].hasOwnProperty(t)) india_timeseries['Dates'][t] = {}
+        india_timeseries['Dates'][t][$(children[1]).text()+'/India'] = {
+                'Confirmed': parseInt($(children[2]).text()),
+                'Recovered': parseInt($(children[3]).text()),
+                'Deaths': parseInt($(children[4]).text())
+        };
+    });
+
+    fs.writeFileSync('data/timeseries_india.json', JSON.stringify(india_timeseries));
+
+    let DATA = india_timeseries;
+
+    function processRow(object, type, row) {
+        let keys = Object.keys(row);
+        if (row[keys[1]] == 'India') return; // Skip India as instead the data is being taken from MOHFW website
+
+        let location = row[keys[0]]+'/'+row[keys[1]];
+        if (!object.Meta.Locations.hasOwnProperty(location)) {
+            object.Meta.Locations[location] = {
+                Country: row[keys[1]],
+                Lat: row[keys[2]],
+                Long: row[keys[3]]
+            }
+        }
+
+        for (let i=4; i < keys.length; i++) {     
+            let d = new Date(keys[i]);
+            d = addZ(d.getDate())+'-'+addZ((d.getMonth()+1))+'-'+d.getFullYear();
+
+            if (!object['Dates'].hasOwnProperty(d)) object['Dates'][d] = {};
+            let k = row[keys[0]]+'/'+row[keys[1]];
+
+            if (!object['Dates'][d].hasOwnProperty(k))
+                object['Dates'][d][k] = {[type]: +row[keys[i]]};
+            else
+                object['Dates'][d][k][type] = +row[keys[i]];
+        }
+    }
+
+    csv.parseString(c.data, {headers:true})
+        .on('data', (row) => processRow(DATA, 'Confirmed', row))
+        .on('end', () => {
+            csv.parseString(r.data, {headers:true}).on('data', row => processRow(DATA, 'Recovered', row))
+                .on('end', () => {
+                    csv.parseString(d.data, {headers: true}).on('data', row => processRow(DATA, 'Deaths', row))
+                    .on('end', () => fs.writeFileSync('data/timeseries_global.json', JSON.stringify(DATA)))
+                });
+        });
+        
+})).catch(error => {
+       console.log(error);
+});
+
