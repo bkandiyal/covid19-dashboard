2465912e5ec6f42520210d6ed42d06a26fd3ccb5
Bhaskar Kandiyal <bkandiyal@gmail.com>
2020-05-05 17:09:07 +0530

    Fixed wrong publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/bkandiyal/covid19-dashboard/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/covid19-dashboard/' : '/',
   transpileDependencies: [
     'vuetify'
   ]
